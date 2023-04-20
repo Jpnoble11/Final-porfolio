cc7a6e5827e2919b9f0d487e932b9db7b039c3a7
Jpnoble1 <noblejohnpaul1o@gmail.com>
2023-04-20 11:47:23 +0800

    need to fix design

--- js/gallery.js
+++ js/gallery.js
@@ -16,11 +16,9 @@ sr.reveal(".logo", { delay: 200, origin: "left" });
 sr.reveal(".navbar", { delay: 400, origin: "top" });
 sr.reveal(".main", { delay: 520, origin: "right" });
 
-sr.reveal(".home-text span", { delay: 600, origin: "top" });
-sr.reveal(".home-text h1", { delay: 680, origin: "left" });
-sr.reveal(".home-text p", { delay: 750, origin: "right" });
-sr.reveal(".main-btn", { delay: 860, origin: "left" });
-sr.reveal(".home-img", { delay: 950, origin: "right" });
+sr.reveal(".info", { delay: 200, origin: "left" });
 
-sr.reveal(".social", { delay: 950, origin: "bottom" });
-sr.reveal(".share p", { delay: 950, origin: "bottom" });
+sr.reveal(".education h1", { delay: 200, origin: "top" });
+
+sr.reveal(".timeline", { delay: 200, origin: "top" });
+timeline;
