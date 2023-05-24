ac45cd9a98f41ade5afcee94e21aff1a2fba62be
Jpnoble1 <noblejohnpaul1o@gmail.com>
2023-05-24 13:37:15 +0800

    not yet done

--- js/home.js
+++ js/home.js
@@ -1,21 +1,10 @@
-let menu = document.querySelector("#menu-icon");
-let navbar = document.querySelector(".navbar");
-
-menu.onclick = () => {
-  menu.classList.toggle("bx-x");
-  navbar.classList.toggle("open");
-};
-
 const sr = ScrollReveal({
   distance: "40px",
   duration: 2500,
   reset: true,
 });
 
-sr.reveal(".logo", { delay: 200, origin: "left" });
-sr.reveal(".navbar", { delay: 400, origin: "top" });
-sr.reveal(".main", { delay: 520, origin: "right" });
-
+sr.reveal("header", { delay: 600, origin: "top" });
 sr.reveal(".home-text span", { delay: 600, origin: "top" });
 sr.reveal(".home-text h1", { delay: 680, origin: "left" });
 sr.reveal(".home-text p", { delay: 750, origin: "right" });
@@ -23,4 +12,8 @@ sr.reveal(".main-btn", { delay: 860, origin: "left" });
 sr.reveal(".home-img", { delay: 950, origin: "right" });
 
 sr.reveal(".social", { delay: 950, origin: "bottom" });
-sr.reveal(".share p", { delay: 950, origin: "bottom" });
+sr.reveal("p", { delay: 950, origin: "bottom" });
+
+sr.reveal(".bg", { delay: 950, origin: "right" });
+sr.reveal(".contactForm", { delay: 950, origin: "right" });
+sr.reveal("h3, h2, .icon", { delay: 950, origin: "right" });
